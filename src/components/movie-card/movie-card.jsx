5041757a1ea5f5cb82fc471acc9a4561d6a4adf6
t22n84r <81875062+t22n84r@users.noreply.github.com>
2023-08-17 05:06:45 -0400

    Full-Stack Immersion Achievement-3 Exercise-3.4
    https://careerfoundry.com/en/steps/react-lifecycle#task
    
    Updated package.json with dependency prop-types
    
    Updated movie-card.jsx to implement prop validation
    
    Updated main-view.jsx to implement data fetch from API & show similar movies name in movie view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,56 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = (props) => {
-    return (
-      <div
-        onClick={() => {
-          props.onMovieClick(props.movieCard);
-        }}
-      >
-        <p> {props.movieCard.title} </p>
+
+  return (
+
+    <div
+      onClick={() => {
+        props.onMovieClick(props.movieCard);
+      }}
+    >
+
+      <div>
+        <p>{props.movieCard.title}</p>
       </div>
-    );
-  };
-  
+
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+
+  movieCard: PropTypes.shape ({
+
+    id: PropTypes.string.isRequired,
+
+    title: PropTypes.string.isRequired,
+
+    description: PropTypes.string.isRequired,
+
+    director: PropTypes.shape ({
+
+      name: PropTypes.string.isRequired,
+
+      bio: PropTypes.string.isRequired,
+      
+      birthyear: PropTypes.string.isRequired,
+
+      deathyear: PropTypes.string.isRequired
+    }),
+
+    genre: {
+
+      name: PropTypes.string.isRequired,
+
+      description: PropTypes.string.isRequired
+    },
+
+    imageurl: PropTypes.string.isRequired,
+
+    featured: PropTypes.bool.isRequired
+
+  }).isRequired,
+
+  onMovieClick: PropTypes.func.isRequired
+};
